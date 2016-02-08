e2ffb5ee56c998bd4049d2e4ec098a5ec66b53d4
Olivier <olivier.duhart@sage.com>
2016-02-08 11:42:53 +0100

    comments

--- rpn.hs
+++ rpn.hs
@@ -1,9 +1,16 @@
 import System.Environment
 import Data.List
 import Debug.Trace
+{-
+**************************************
+ RPN (Reverse Polish Notation) evaluator
+**************************************
+-}
 
 
-
+{-
+	returns True if str represents a number
+-}
 isNumber :: String -> Bool
 isNumber str =
     case (reads str) :: [(Double, String)] of
@@ -11,6 +18,11 @@ isNumber str =
       _ -> False
 
 
+{-
+	returns True if str represents an Operation,
+			False if str is a number
+			throws an error otherwise	
+-}	  
 isOperation :: String -> Bool
 isOperation op 
     | elem op ["+","-","*","/"] = True
@@ -21,32 +33,28 @@ isOperation op
 
 
 
-
+{-
+	Split an expression (represented as a string array) into a tuple containing
+	    - a number stack (as string array)
+		- a operation stack (as string array)
+-}
 splitExpression :: [String] -> ([String],[String])
 splitExpression expr = ((filter isNumber expr), (filter isOperation expr))
+
+
 
 convert :: ([String],[String]) -> ([Float],[String]) 
 convert (n,o) = ((map (read::String->Float) n),o) 
 
+{-
+	parse a string into a double stack
+		- a number stack (as double array)
+		- an operation stack (as string array)
+-}
 parse :: String -> ([Float],[String])
 parse code = convert (splitExpression (words code))
 
-{-
-main loop
-   - nums : Float stack
-   - ops : operations stack
--}
-eval :: [Float] -> [String] -> Float
-eval nums ops
-	| null ops && (length nums > 1) = error "bad expression, unable to evaluate (missing operator ?)"
-	| null ops = traceShow("no more op: ",nums) head nums
-	| (length ops >= 1) && (length nums >= 2) = eval ([(evaluate (head ops) (take 2 nums))]++(drop 2 nums)) (tail ops)
-	| otherwise = error "bad expression, unable to evaluate"
 
-rpn :: String -> Float
-rpn expr = eval n o
-	where (n,o) = parse expr
-	
 {-
 single operation evaluation
 	- op : operation string
@@ -59,6 +67,29 @@ evaluate op args
    | op == "*" = traceShow ("* "++(show args)) args!!0 * args!!1
    | op == "/" = traceShow ("/ "++(show args)) args!!0 / args!!1
    | otherwise = error ("unknown operation : " ++ op)
+
+
+{-
+main loop : evaluate a double stack 
+   - nums : Float stack
+   - ops : operations stack
+-}
+eval :: [Float] -> [String] -> Float
+eval nums ops
+	| null ops && (length nums > 1) = error "bad expression, unable to evaluate (missing operator ?)"
+	| null ops = traceShow("no more op: ",nums) head nums
+	| (length ops >= 1) && (length nums >= 2) = eval ([(evaluate (head ops) (take 2 nums))]++(drop 2 nums)) (tail ops)
+	| otherwise = error "bad expression, unable to evaluate"
+
+{-
+	entry point : evaluate an expression 
+	   - expr t: the expression as a string
+-}
+rpn :: String -> Float
+rpn expr = eval n o
+	where (n,o) = parse expr
+	
+
   
 
    
