ffee21700e453f1611f91ce2dd60df81bd206ee5
Olivier <olivier.duhart@sage.com>
2016-02-08 11:32:20 +0100

    improvement

--- rpn.hs
+++ rpn.hs
@@ -2,26 +2,25 @@ import System.Environment
 import Data.List
 import Debug.Trace
 
-myProduct :: (Num a) => [a] -> a
-myProduct = foldr1 (*)    
-
-fact2 :: (Integral a) => a -> a
-fact2 n = myProduct [1..n] 
-
 
 
 isNumber :: String -> Bool
 isNumber str =
     case (reads str) :: [(Double, String)] of
       [(_, "")] -> True
-      _         -> False
+      _ -> False
 
-
-prse ::  [String] -> [String]
-prse = takeWhile isNumber
 
 isOperation :: String -> Bool
-isOperation str = str == "+" || str == "-" || str == "*" || str == "/"
+isOperation op 
+    | elem op ["+","-","*","/"] = True
+    | isNumber op = False
+    | otherwise = error(op ++ " :: bad operator") False
+--isOperation op = str == "+" || str == "-" || str == "*" || str == "/"
+--isOperation op =  
+
+
+
 
 splitExpression :: [String] -> ([String],[String])
 splitExpression expr = ((filter isNumber expr), (filter isOperation expr))
@@ -32,45 +31,36 @@ convert (n,o) = ((map (read::String->Float) n),o)
 parse :: String -> ([Float],[String])
 parse code = convert (splitExpression (words code))
 
+{-
+main loop
+   - nums : Float stack
+   - ops : operations stack
+-}
 eval :: [Float] -> [String] -> Float
 eval nums ops
-	| null ops = head nums
+	| null ops && (length nums > 1) = error "bad expression, unable to evaluate (missing operator ?)"
+	| null ops = traceShow("no more op: ",nums) head nums
 	| (length ops >= 1) && (length nums >= 2) = eval ([(evaluate (head ops) (take 2 nums))]++(drop 2 nums)) (tail ops)
-	| otherwise = error "bad expression, unaleb to evaluate"
+	| otherwise = error "bad expression, unable to evaluate"
 
 rpn :: String -> Float
 rpn expr = eval n o
 	where (n,o) = parse expr
-
-{- operation String -> Float -> Float -> Float
-evaluate op args
-   | op == "+" = (+)
-   | op == "-" = (-)
-   | op == "*" = (*)
-   | op == "/" = (/)
-   | otherwise = error ("unknown operation : " ++ op) -}
-   
-    
 	
+{-
+single operation evaluation
+	- op : operation string
+	- args : 2 float array
+-}	
 evaluate :: String -> [Float] -> Float	
 evaluate op args
    | op == "+" = traceShow ("+ "++(show args)) args!!0 + args!!1
-   | op == "-" = traceShow ("- "++(show args))args!!0 - args!!1
-   | op == "*" = traceShow ("* "++(show args))args!!0 * args!!1
-   | op == "/" = traceShow ("/ "++(show args))args!!0 / args!!1
+   | op == "-" = traceShow ("- "++(show args)) args!!0 - args!!1
+   | op == "*" = traceShow ("* "++(show args)) args!!0 * args!!1
+   | op == "/" = traceShow ("/ "++(show args)) args!!0 / args!!1
    | otherwise = error ("unknown operation : " ++ op)
-
-	
-
--- | otherwise = error ("bad oper " ++ op)
-	
   
-solveRPN :: (Num a, Read a) => String -> a  
-solveRPN = head . foldl foldingFunction [] . words  
-    where   foldingFunction (x:y:ys) "*" = (x * y):ys  
-            foldingFunction (x:y:ys) "+" = (x + y):ys  
-            foldingFunction (x:y:ys) "-" = (y - x):ys  
-            foldingFunction xs numberString = read numberString:xs  	
+
    
 main = do
 	expr <- getLine
