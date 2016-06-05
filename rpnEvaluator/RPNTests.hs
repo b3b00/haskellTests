deb5b49ad3c2f760b6b8b583811c79575845b9f5
Olivier Duhart <olivier.duhart@sage.com>
2016-06-05 19:24:13 +0200

    add factorial Unit Test

--- rpnEvaluator/RPNTests.hs
+++ rpnEvaluator/RPNTests.hs
@@ -21,22 +21,25 @@ tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2]
 main :: IO Counts
 main =  runTestTT tests-}
 module Main where
+import Control.Exception
+import Control.Monad    
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.HUnit
 import RPN
 
 
-testSplitExpression = (splitExpression (words " 2 2 +")) == (["2","2"],["+"])    
 
---test1 :: Test
-test1 = TestCase  $ (assertEqual "for (splitExpression 2 2 +)" (["2","2"],["+"]) (splitExpression (words " 2 2 +")))
 
 --test2:: Test
-test2 = TestCase $ (assertEqual "for (computeRPN \"2 2 +\")" (4.0) (computeRPN "2 2 +"))
+test1 = TestCase $ (assertEqual "for (computeRPN \"2 2 +\")" (4.0) ((computeRPN "2 2 +")))
+
+test2 = TestCase $ ( assertEqual "for (computeRPN 3 3 3 3 S)" (12.0) ((computeRPN"3 3 3 3 S"))) 
 
 
+test3 = TestCase $ ( assertEqual "for (computeRPN 5.0 !)" (5.0*4.0*3.0*2.0*1.0) ((computeRPN"3 3 3 3 S"))) 
 
+--test3 = TestCase $ ( assertException ErrorCall (evaluate $ computeRPN " 2 2 X"))
 
 
 
