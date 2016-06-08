05e085cfc8f4b643ff40943c84a0685cf8824f84
Olivier Duhart <olivier.duhart@sage.com>
2016-06-08 10:20:35 +0200

    ajout de TU parse et run des expressions

---
+++ parsec/BoolExprTests.hs
@@ -0,0 +1,34 @@
+
+module Main where
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Test.HUnit
+import Ast
+import Runtime
+
+tAndt = BBinary And (BoolConst True) (BoolConst True)
+
+test1 = TestCase  $ (assertEqual "true and true" (True) (fst (evalBExpr tAndt [])))
+
+notT = Not (BoolConst True) 
+
+test2 = TestCase  $ (assertEqual "not true" (False) (fst (evalBExpr notT [])))
+
+tOrf = BBinary Or (BoolConst True) (BoolConst False)
+
+test3 = TestCase  $ (assertEqual "true and true" (True) (fst (evalBExpr tOrf [])))
+
+
+greater = RBinary Greater (IntConst 1) (IntConst 2)
+
+test4 = TestCase  $ (assertEqual "1 > 2" (False) (fst (evalBExpr greater [])))
+
+less = RBinary Less (IntConst 1) (IntConst 2)
+
+test5 = TestCase  $ (assertEqual "1 < 2" (True) (fst(evalBExpr less [])))
+
+-- hUnitTestToTests: Adapt an existing HUnit test into a list of test-framework tests
+tests = hUnitTestToTests $ TestList [TestLabel "and test" test1, TestLabel "not test" test2, TestLabel "or test" test3, TestLabel "greater test" test4, TestLabel "less test" test5]
+
+main = defaultMain tests          
+        
--- parsec/Runtime.hs
+++ parsec/Runtime.hs
@@ -2,8 +2,43 @@ module Runtime where
 import System.IO
 import Control.Monad
 import Ast
+import Debug.Trace
+
+
+--play :: Stmt -> List -> IO()
+play stmt map = do
+    putStrLn(show(stmt))
 
 run :: Stmt -> IO()
 run stmt = do
-    putStrLn (show(stmt))
-    
+    play stmt []
+
+evalBExpr :: BExpr -> [a] -> (Bool,[a])
+evalBExpr e map = case e of
+    BoolConst b -> (b,map)
+    Not expr ->  do((not(fst(evalBExpr expr map))),(snd (evalBExpr expr map)))                
+    BBinary op left right -> evalBBoolOperator op left right map
+    RBinary op left right -> evalRBoolOperator op left right map
+
+evalBBoolOperator :: BBinOp -> BExpr -> BExpr -> [a] -> (Bool, [a])
+evalBBoolOperator op left right map = case op of
+    And  -> ((fst (evalBExpr left map)) && (fst (evalBExpr right map)), ((snd (evalBExpr left map)) ++ (snd (evalBExpr right map))))
+    Or  -> ((fst (evalBExpr left map)) || (fst (evalBExpr right map)), ((snd (evalBExpr left map)) ++ (snd (evalBExpr right map))))
+
+evalRBoolOperator :: RBinOp -> AExpr -> AExpr -> [a] -> (Bool, [a])
+evalRBoolOperator op left right map = case op of
+    Greater -> ((fst (evalAExpr left map)) > (fst (evalAExpr right map)), (snd (evalAExpr left map)) ++ (snd (evalAExpr right map)))    
+    Less -> ((fst (evalAExpr left map)) < (fst (evalAExpr right map)), (snd (evalAExpr left map)) ++ (snd (evalAExpr right map))) 
+
+              
+evalAExpr :: AExpr -> [a] -> (Integer, [a])
+evalAExpr e context = case e of
+    IntConst i -> (i,context)
+    Neg expr -> do
+        (fst (evalAExpr expr context) , snd(evalAExpr expr context))        
+        --
+        --(fst res,snd res) -- negate expr        -}
+    ABinary op left right -> (-42, context) -- do operation
+    Var name -> (-78, context) -- lookup value in context and return it 
+
+
