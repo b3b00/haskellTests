168b28a7347fc131cb70619a12bfe2322a0ff671
Olivier Duhart <olivier.duhart@sage.com>
2016-06-14 12:58:17 +0200

    correct MOV instrcution

--- parsec/Runner.hs
+++ parsec/Runner.hs
@@ -35,32 +35,44 @@ setInHeap :: Int -> StackValue -> [StackValue] -> [StackValue]
 setInHeap address value heap = replaceNth address value heap
 
 
-mov :: Machine -> Int -> (Int, Machine)
-mov machine ip = 
-    let  debug = trace ("in mov op ") 1 in  
+movOp :: Machine -> Int -> (Int, Machine)
+ 
+movOp machine ip = 
+    let  debug = trace ("\nin mov op ") 1 in  
         let popped = trace "mov pop" popValue (stack machine) in
             let  value = trace ("mov v"++(show (fst popped))) (fst popped) in
-                let addr = trace "mov a" (bytecode machine) !! (ip+1) in
-                    let newheap = trace "mov h" setInHeap addr value (heap machine) in
+                let addr = trace ("mov a val::"++(show value)) (bytecode machine) !! (ip+1) in
+                    let newheap = trace ("mov h "++(show value)++" -> @"++(show addr)) setInHeap (addr-1) value (heap machine) in
                         let newStack = (snd popped) in
                         --trace ("mov "++(show value)++" @ "++(show addr)) (ip+2,Machine (bytecode machine) newStack newheap (heapAddresses machine))
                             (ip+2,Machine (bytecode machine) newStack newheap (heapAddresses machine))
-    
+
+{-
+movOp machine ip = 
+    let  debug = trace "\ntu vas la bouger ta graisse !!!" 1 in          
+        trace "mouarf" (ip+2,machine)                            
+-}
+
+
 pushOp :: Machine -> Int -> (Int,Machine)
 pushOp machine ip = 
-    let k = (ip+1) in
-        let addr = (bytecode machine) !! k  in                        
-            let value = (heap machine) !! addr  in
-                let newStack = pushValue (stack machine) value in
-                    (ip+2,(Machine (bytecode machine) newStack (heap machine) (heapAddresses machine)))
+    let debug = trace ("\nin push "++(show machine)) 1 in 
+        let k = trace ("push op ip+1="++(show (ip+1))) (ip+1) in
+            let addr = trace ("push op get head address... heap["++(show k)++"-1]") ((bytecode machine) !! (k-1)) in -- trace ("push op get head address... heap["++(show (k-1))++"] ")  in                        
+                let value = trace ("push op v from head@"++(show (addr-1))) (heap machine) !! (addr-1)  in
+                    let newStack = trace ("push op v = "++(show value)) pushValue (stack machine) value in
+                        trace ("push -> stack="++(show newStack))(ip+2,(Machine (bytecode machine) newStack (heap machine) (heapAddresses machine)))
 
 runIt :: (Int, Machine) -> (Int, Machine)
 runIt machineState 
-    | (fst machineState) > (length (bytecode (snd machineState))) = machineState
+    | (fst machineState) >= (length (bytecode (snd machineState))) = machineState
     | otherwise = case ((bytecode (snd machineState)) !! (fst machineState)) of 
-        1 -> trace ("push @ "++(show (fst machineState))++" "++(show (snd machineState))++"\n") runIt (pushOp (snd machineState) (fst machineState))
-        3 -> trace ("mov @ "++(show (fst machineState))++" "++(show (snd machineState))++"\n")  runIt (mov (snd machineState) (fst machineState))
-        _ -> trace ("dont know ["++(show ((bytecode (snd machineState)) !! (fst machineState)))++"] @"++(show (fst machineState))++" "++(show (snd machineState))) (-1,(Machine [] [] [] [] ))
+        1 -> trace ("\nPUSH @ "++(show (fst machineState))++" "++(show (snd machineState))++"\n") runIt (pushOp (snd machineState) (fst machineState))
+        3 -> let movIt = (movOp (snd machineState) (fst machineState)) in
+            trace ("\nMOV => "++(show (fst movIt))++" "++(show (snd movIt))++"\n") runIt movIt
+        --trace ("\nmov @ "++(show (fst machineState))++" "++(show (snd machineState))++"\n")  runIt (movOp (snd machineState) (fst machineState))
+        --3 -> trace ("\nmov @ "++(show (fst machineState))++" "++(show (snd machineState))++"\n") ((fst machineState)+2,(snd machineState))   
+        _ -> trace ("\nDONT KNOWN ["++(show ((bytecode (snd machineState)) !! (fst machineState)))++"] @"++(show (fst machineState))++" "++(show (snd machineState))) (-1,(Machine [] [] [] [] ))
 
 runMachine :: Int ->  Machine -> Machine  
 runMachine ip machine =  snd $ runIt (ip,machine)
