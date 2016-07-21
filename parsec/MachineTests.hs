db6ea1576b52356a91249f640e0252de72c7a6fb
Olivier Duhart <olivier.duhart@sage.com>
2016-07-21 11:57:29 +0200

    Unit Tests

--- parsec/MachineTests.hs
+++ parsec/MachineTests.hs
@@ -6,20 +6,23 @@ import Ast
 import Stack
 import Machine
 import Compiler
+import Text.Parsec.Pos   
 
-expr1 = (IntConst 1)
+pos = (newPos "dummy" 0 0)
 
-test1 = TestCase  $ (assertEqual "const int 1" (Machine 0 [1,0] [] [IntVal 1] []) (compileAExpr expr1 (Machine 0 [] [] [] [])))
+expr1 = (IntConst pos 1)
 
-expr2 = (BoolConst True)
+test1 = TestCase  $ (assertEqual "const int 1" (Machine 0 [1,0] [] [IntVal 1] []) (compileExpr expr1 (Machine 0 [] [] [] [])))
 
-test2 = TestCase  $ (assertEqual "const bool true" (Machine 0 [1,0] [] [BoolVal True] []) (compileBExpr expr2 (Machine 0 [] [] [] [])))
+expr2 = (BoolConst pos True)
 
-expr3 = (ABinary Add (IntConst 2) (IntConst 1))
-test3 = TestCase  $ (assertEqual "int binary op 2+1" (Machine 0 [1,0,1,1,4] [] [IntVal 2, IntVal 1] []) (compileAExpr expr3 (Machine 0 [] [] [] [])))
+test2 = TestCase  $ (assertEqual "const bool true" (Machine 0 [1,0] [] [BoolVal True] []) (compileExpr expr2 (Machine 0 [] [] [] [])))
 
-expr4 = (BBinary And (BoolConst False) (BoolConst True)) 
-test4 = TestCase  $ (assertEqual "int binary op False and true" (Machine 0 [1,0,1,1,10] [] [BoolVal False, BoolVal True] []) (compileBExpr expr4 (Machine 0 [] [] [] [])))
+expr3 = (Binary Add pos (IntConst pos 2) (IntConst pos 1))
+test3 = TestCase  $ (assertEqual "int binary op 2+1" (Machine 0 [1,0,1,1,4] [] [IntVal 2, IntVal 1] []) (compileExpr expr3 (Machine 0 [] [] [] [])))
+
+expr4 = (Binary And pos (BoolConst pos False) (BoolConst pos True)) 
+test4 = TestCase  $ (assertEqual "int binary op False and true" (Machine 0 [1,0,1,1,10] [] [BoolVal False, BoolVal True] []) (compileExpr expr4 (Machine 0 [] [] [] [])))
 
 
 tests = hUnitTestToTests $ TestList [TestLabel "expr const int 1" test1,TestLabel "expr const bool true" test2,TestLabel "expr binary int 2+1" test3,TestLabel "expr binary bool False and True" test4]
--- parsec/StmtTests.hs
+++ parsec/StmtTests.hs
@@ -8,25 +8,25 @@ import Parser
 
 assign = parseString "( toto := 42 )"
 
-test1 = TestCase  $ (assertEqual "toto := 42" ([("toto",42)]) (evalStmt assign []))
+test1 = TestCase  $ (assertEqual "toto := 42" ([("toto",IntegerV 42)]) (evalStmt assign []))
 
 manyAssign = parseString "( toto := 1; titi := 2 )"
 
-test2 = TestCase  $ (assertEqual "toto := 1; titi := 2;" ([("titi",2),("toto",1)]) (evalStmt manyAssign []))
+test2 = TestCase  $ (assertEqual "toto := 1; titi := 2;" ([("titi",IntegerV 2),("toto",IntegerV 1)]) (evalStmt manyAssign []))
 
 ifthenelseTrue = parseString "(   toto := 0;   if (true) then       (toto :=1)     else       (toto := 2) )"
 
 
-test3 = TestCase  $ (assertEqual "toto := 0; if true then toto :=1; else toto := 2;" ([("toto",1)]) (evalStmt ifthenelseTrue [])) 
+test3 = TestCase  $ (assertEqual "toto := 0; if true then toto :=1; else toto := 2;" ([("toto",IntegerV 1)]) (evalStmt ifthenelseTrue [])) 
 
 
 ifthenelseEq = parseString "( toto := 0; if (toto == 0) then (toto :=1) else (toto := 2)) "
 
-test4 = TestCase  $ (assertEqual "toto := 0; if toto == 0 then toto :=1; else toto := 2;" ([("toto",1)]) (evalStmt ifthenelseEq []))
+test4 = TestCase  $ (assertEqual "toto := 0; if toto == 0 then toto :=1; else toto := 2;" ([("toto",IntegerV 1)]) (evalStmt ifthenelseEq []))
 
 while = parseString "toto := 0; while (toto < 10) do (toto := toto + 1)"
 
-test5 = TestCase  $ (assertEqual "toto := 0; while (toto < 10) do (toto := toto + 1)" ([("toto",10)]) (evalStmt while []))
+test5 = TestCase  $ (assertEqual "toto := 0; while (toto < 10) do (toto := toto + 1)" ([("toto",IntegerV 10)]) (evalStmt while []))
 
 tests = hUnitTestToTests $ TestList [TestLabel "assign test" test1,TestLabel "many assign test" test2,TestLabel "if then else test" test3,TestLabel "if then else test with eq" test4,TestLabel "while" test5]
 
